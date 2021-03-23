1b2be90a7f0bda9362b6eabc815eaf23e6d85344
Victor Martinez estrada <vicmarti@c1r3s3.42madrid.com>
2021-03-23 15:24:58 +0100

    Toying

--- checker.h
+++ checker.h
@@ -6,12 +6,13 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 19:32:53 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/03/21 20:05:55 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/03/23 14:51:06 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
+#include "libft/libft.h" //TODO Cp to includes
 
 typedef struct s_psh_swp
 {
--- sources/checker.c
+++ sources/checker.c
@@ -6,13 +6,15 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 19:21:05 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/03/22 15:12:16 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/03/23 15:22:28 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
 #include <limits.h>
+#include <errno.h>
+#include "checker.h"
 
+#include <stdio.h>
 void	error()
 {
 	write(2, "Error\n", 6);
@@ -22,30 +24,24 @@ void	error()
 void	check_args(int argc, char **args)
 {
 	long	num;
+	char	*end;
 
-	while(--argc >= 0)
+	while(argc > 1)
 	{
-		num = atol(args[argc]);//TODO: STRTOL
-		if (num > INT_MAX || num < INT_MIN)
+		end = args[argc];
+		num = ft_strtol(args[argc], end);
+		if (*end || (num == 0 && errno == EINVAL) ||
+			num > INT_MAX || num < INT_MIN)
 			error();
-
+		else
+			printf("Num is: %ld\n", num);
+		argc--;
 	}
 }
 
 int	main(int argc, char **args)
 {
-	t_psh_swp	ps;
-
-	ps.elems = argc;
-	ps.top_a = argc;
-	ps.a = calloc(argc * 2, sizeof(int *));
-	if (!ps.a)
-		error();
-	while (argc-- > 0)
-	{
-		ps.a[ps.elems - argc] = atoi(args[argc]);
-		if (ps.a[ps.elems] == 
-
+	check_args(argc, args);
 	exit(0);
 }
 
