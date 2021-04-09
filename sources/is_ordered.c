8316127c353ce61e0501419a784d388229fc2255
Victor Martinez estrada <vicmarti@c1r3s6.42madrid.com>
2021-04-09 16:09:22 +0200

    Ordered loop updates properly. (Facepalm)

--- sources/is_ordered.c
+++ sources/is_ordered.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 12:48:17 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/03/30 13:33:09 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/04/09 16:05:06 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	is_ordered(t_psh_swp game)
 	{
 		if (game.stack[0] > game.stack[1])
 			return (0);
+		game.stack++;
 	}
 	return (1);
 }
